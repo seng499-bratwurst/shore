a519db73796fbcfe0c9a5ad3a36cb202d41958d1
jaynaAutonommi <jayna@autonommi.com>
2025-05-30 18:09:00 -0700

    Add Initial Navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Fira_Code, Inter } from 'next/font/google';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './globals.css';
+import Navbar from '@/components/nav/navbar';
 
 export const metadata: Metadata = {
   title: 'Shore',
@@ -26,7 +27,10 @@ export default function RootLayout({
   }
   return (
     <html lang="en">
-      <body className={`${inter.variable} ${firaCode.variable}`}>{children}</body>
+      <body className={`${inter.variable} ${firaCode.variable}`}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/nav/navbar.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Link from 'next/link';
+import { Button } from '../ui/button/button'
+import onc_logo from './onc_logo.png';
+import Image from 'next/image';
+
+
+export default function Navbar() {
+  return (
+    <nav className="flex items-center justify-between p-4 bg-[var(--color-neutral-800)] w-full h-16 relative">
+      <div className="flex items-center">
+        <Image src={onc_logo} alt="Ocean Networks Canada" className="h-16 w-auto mr-4 "  />
+        <h1 className="text-2xl text-[var(--color-neutral-100)] font-bold">Astrolabe</h1>
+      </div>
+      <div className="space-x-4">
+        <Link className="text-[var(--color-neutral-100)] " href="/login">
+            Log In
+        </Link>
+        <Link href="/login">
+            <Button className="bg-[var(--color-primary-400)] text-white px-4 py-2 rounded">Register</Button>
+        </Link>
+      </div>
+    </nav>
+  );
+}
