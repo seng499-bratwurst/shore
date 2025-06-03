f11afc7c5074af952c3d8a9a1405536d2a61f491
jaynaAutonommi <jayna@autonommi.com>
2025-06-03 16:42:53 -0700

    fix styling

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,11 +3,11 @@ import { Button } from '@/components/ui/button/button';
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-[var(--color-primary-50)]  to-[var(--color-primary-300)]  text-text-[var(--color-neutral-900)] flex flex-col items-center justify-center  p-4">
+    <div className="min-h-screen bg-gradient-to-b from-primary-50 to-primary-300 text-neutral-900 flex flex-col items-center justify-center p-4">
       {/* Main Content */}
       <div className="text-center max-w-[42rem]">
         {/* Purple Title Box */}
-        <h1 className="bg-[var(--color-secondary-400)] text-2xl font-bold py-2 px-4 mb-4 items-center ">
+        <h1 className="bg-secondary-400 text-2xl font-bold py-2 px-4 mb-4 items-center ">
           Where Ocean Data Meets Intelligence
         </h1>
 
@@ -18,7 +18,7 @@ export default function Home() {
 
         {/* Blue Chat Button */}
         <a href="/chat">
-          <Button className="bg-[var(--color-primary-400)] text-[var(--color-neutral-900)]  font-semibold py-2 px-6 rounded hover:bg-cyan-600 transition">
+          <Button className="bg-brand-primary text-neutral-900  font-semibold py-2 px-6 rounded hover:bg-primary-600 transition">
             Start Chat Now
           </Button>
         </a>
