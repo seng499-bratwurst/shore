e2a4b1b76b6bb38f85d458ad595c2eae7ce7f1bc
Jayna Bettesworth <bettesworthjayna@gmail.com>
2025-06-10 14:48:42 -0700

    Update with new styles

--- src/components/nav/navbar.tsx
+++ src/components/nav/navbar.tsx
@@ -6,17 +6,17 @@ import Image from 'next/image';
 
 export default function Navbar() {
   return (
-    <nav className="flex items-center justify-between p-4 bg-neutral-100 w-full h-16 relative shadow-sm">
+    <nav className="flex items-center justify-between p-4 bg-primary-50 dark:bg-primary-900 w-full h-16 relative shadow-sm">
       <div className="flex items-center">
         <Image src={'/onc_logo.png'} width={64} height={64} alt="Ocean Networks Canada" className="h-16 w-auto mr-4 "  />
-        <h1 className="text-2xl text-neutral-900 font-bold">Astrolabe</h1>
+        <h1 className="text-2xl text-neutral-900 dark:text-neutral-50 font-bold">Astrolabe</h1>
       </div>
       <div className="space-x-4">
-        <Link className="text-neutral-900" href="/login">
+        <Link className="text-neutral-900 dark:text-neutral-50" href="/login">
             Log In
         </Link>
         <Link href="/login">
-            <Button className="bg-primary-400 text-neutral-900 px-4 py-2 rounded">Register</Button>
+            <Button className="bg-primary-500 text-neutral-900 px-4 py-2 rounded">Register</Button>
         </Link>
       </div>
     </nav>
