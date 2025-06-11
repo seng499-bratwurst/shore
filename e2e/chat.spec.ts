3219aa79c226b64f33511d04373f35ca4fecd00d
Robbie <rthomlinson@uvic.ca>
2025-06-11 13:32:42 -0700

    chat page e2e test

--- e2e/chat.spec.ts
+++ e2e/chat.spec.ts
@@ -8,11 +8,11 @@ describe('Chat Page', () => {
     await page.goto('/');
   });
 
-  test('homepage has expected title and content', async ({ page }) => {
+  test('chat page has expected title and content', async ({ page }) => {
     await expect(page).toHaveTitle(/shore/i);
   });
 
-  test('homepage is accessible', async ({ page }) => {
+  test('chat page is accessible', async ({ page }) => {
     const accessibilityScanResults = await new AxeBuilder({ page })
       .withTags(['wcag2a', 'wcag2aa', 'best-practice'])
       .analyze();
