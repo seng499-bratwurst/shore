dd7df35a0c979eeec9d217b404dbd957bcc9341b
Robbie <rthomlinson@uvic.ca>
2025-07-25 09:41:47 -0700

    added development and prod URL's based on env

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,11 @@
 import axios from 'axios';
 
+const PROD_BASE_URL = 'https://astrolabe.coursesystem.app/api/';
+const DEV_BASE_URL = 'http://localhost:5000/api/';
+
 // An axios instance configured for the Rift backend
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/',
+  baseURL: process.env.NODE_ENV === 'production' ? PROD_BASE_URL : DEV_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     'Access-Control-Allow-Origin': '*', // Adjust this for production
